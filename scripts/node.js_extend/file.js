0fb0314e03cd66ee6d31323087b52c3c03dbd81d
Jason-Zhao-Jie <261343578@qq.com>
2016-08-11 23:36:40 +0800

    Arrange node.js modules

---
+++ scripts/node.js_extend/file.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Jason-Zhao-Jie on 2016/7/20.
+ */
+
+(function() {
+    if (libArmyAnt.nodeJs) {
+
+        this.libArmyAnt.File = this.libArmyAnt.Object.Inherit({
+
+
+            ctor:function(){
+
+            }
+            
+        });
+
+    }
+    this.libArmyAnt._onInited();
+})();
